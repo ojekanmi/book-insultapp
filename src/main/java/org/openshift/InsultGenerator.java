579358b3509bfe5abd6d5508a29e8127746981d2
admin <ojekanmi@gmail.com>
2020-08-08 21:59:05 -0400

    linked with REST455

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -13,7 +13,7 @@ try {
 databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
 databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
 String username = System.getenv("POSTGRESQL_USER");
-String password = System.getenv("PGPASSWORDS");
+String password = System.getenv("PGPASSWORD");
 Connection connection = DriverManager.getConnection(databaseURL, username,
 password);
 if (connection != null) {
